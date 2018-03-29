d479879a561cf7140551aa25395420728b0344e1
jdavid <jdavid@debian>
2018-03-29 11:28:46 -0500

    Etapa 3 psinfo

--- psinfo.c
+++ psinfo.c
@@ -16,23 +16,25 @@ typedef struct{
 
 void obtenerInformacion(proceso*,char[10]);
 void imprimirInformacion(proceso*);
+void obtenerInformacionProceso(char cadena[1000],proceso *pr, char nroProceso[10]);
 
 int main(int arg, char **args){
     if(strcmp(args[1],"-l")==0){
         proceso *proce;
         printf("-- Información recolectada!!!\n\n");
+
         for(int i = 2; i<arg; i++){
             printf("Pid: %s\n",args[i]);
             proce = (proceso*)malloc(sizeof(proceso));        
             obtenerInformacion(proce,args[i]);              
-            imprimirInformacion(proce);   
+            imprimirInformacion(proce);
             printf("\n\n");
             free(proce);
         }
 
     }
     else if(strcmp(args[1],"-r")==0){
-        FILE *fl;
+        
         
         char ruta[100]="psinfo-report-";
         for(int i = 2; i<arg; i++){
@@ -40,21 +42,24 @@ int main(int arg, char **args){
             strcat(ruta, "-");            
         }
         strcat(ruta, ".info");
-        fl = fopen(ruta,"w");
 
+        char cadena[1000];
         proceso *proce;
 
+        FILE *fl;
+        fl = fopen(ruta,"wt");
+        
         for(int i = 2; i<arg; i++){
             proce = (proceso*)malloc(sizeof(proceso));        
             obtenerInformacion(proce,args[i]);  
-            //falta recorrer las estructuras para guardarlas en el archivo
-
-            fwrite( cadena, sizeof(char), sizeof(cadena), fp );     
-
+            obtenerInformacionProceso(cadena, proce,args[i]);        
+            
+            fputs(cadena,fl);
+            strcpy(cadena,""); 
             free(proce);     
         }
-
         fclose(fl);
+        printf("Archivo de salida generado: %s\n",ruta);
     }
     else if(arg > 1){        
         proceso pr;        
@@ -126,3 +131,38 @@ void imprimirInformacion(proceso *pr){
     printf("Número de cambios de contexto realizados (voluntarios - no voluntarios): %s - %s\n",pr->cv,pr->cnv);
 
 }
+
+void obtenerInformacionProceso(char cadena[1000], proceso *pr, char nroProceso[10]){
+    //char cadena[1000];
+    strcat(cadena,"Pid: ");
+    strcat(cadena,nroProceso);
+    strcat(cadena,"\nNombre del proceso: ");
+    strcat(cadena,pr->nombre);
+    strcat(cadena,"\nEstado del proceso: ");
+    strcat(cadena,pr->estado);
+     if(strcmp(pr->memoria,"-1")==0){
+        strcat(cadena,"\nLa información de memoria no se encuentra disponible en este proceso.");
+        strcat(cadena,pr->memoria);
+    }
+    else{
+        strcat(cadena,"\nTamaño total de la imagen de memoria: ");
+        strcat(cadena,pr->memoria);
+        strcat(cadena," kB");
+        strcat(cadena,"\n\tTamaño de la memoria en la región TEXT: ");
+        strcat(cadena,pr->text);
+        strcat(cadena," kB");
+        strcat(cadena,"\n\tTamaño de la memoria en la región DATA: ");
+        strcat(cadena,pr->data);
+        strcat(cadena," kB");
+        strcat(cadena,"\n\tTamaño de la memoria en la región STACK: ");
+        strcat(cadena,pr->stack);
+        strcat(cadena," kB");
+    }
+    strcat(cadena,"\nNúmero de cambios de contexto realizados (voluntarios - no voluntarios): ");
+    strcat(cadena,pr->cv);
+    strcat(cadena," - ");
+    strcat(cadena,pr->cnv);
+    strcat(cadena,"\n\n");
+    //return cadena;
+}
+
